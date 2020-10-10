d8aa638d253c687056447915ce1ca598aa82b2a4
saba negatu <sabetoab@yahoo.com>
2020-10-10 15:25:58 -0400

    added all new files

---
+++ build/precache-manifest.42dbf9183c4bde8f0caf184e8a8e053a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "8b30f8eff6f050c4c8ca",
+    "url": "/static/js/main.8b30f8ef.chunk.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "8b30f8eff6f050c4c8ca",
+    "url": "/static/css/main.1d367a25.chunk.css"
+  },
+  {
+    "revision": "8cd54a94ecf954e8a612a64e095a521b",
+    "url": "/index.html"
+  }
+];
